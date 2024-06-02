e79a3fccbc880ff319e8db395673f4f31db6f69f
Nicolai Ringbæk <nicolai.ringbaek@outlook.com>
2024-06-02 03:42:35 +0200

    updated identity

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nicolai ~ Blog';
+export const SITE_DESCRIPTION = 'My personal blog on anything tech and programming related';
+export const GITHUB_LINK = 'https://github.com/nringbaek/blog';
